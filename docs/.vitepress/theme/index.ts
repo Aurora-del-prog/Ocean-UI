b69679e999b42e1d512952164add23a878e85f1d
Cc <2744166358@qq.com>
2023-07-23 22:08:47 +0800

    初始化配置

---
+++ docs/.vitepress/theme/index.ts
@@ -0,0 +1,13 @@
+import Theme from 'vitepress/theme'
+
+import type {App} from 'vue'
+
+export default {
+  ...Theme,
+  // 扩展应用程序实例
+  // enhanceApp({app}:{app:App<never>}) {
+  //   // 注册组件
+  //   registerComponents(app)
+  //   app.use(SheepUI)
+  // }
+}
---
+++ docs/components/button/index.md
@@ -0,0 +1,4 @@
+# 按钮 - Button
+:::demo 这是SButton组件，可以通过传递type来改变它的类型
+
+:::
