29ccf99bfdcef4766258c32ab278899a47be5597
Cc <2744166358@qq.com>
2023-07-25 22:17:13 +0800

    feat: 🎸 add support for unit testing with vitest

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+/// <reference types="vitest" />
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import vueJSX from "@vitejs/plugin-vue-jsx";
@@ -5,4 +6,14 @@ import vueJSX from "@vitejs/plugin-vue-jsx";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), vueJSX()],
+  test: {
+    // jest like test apis
+    globals: true,
+    // 模拟dom环境
+    environment: 'happy-dom',
+    // 支持tsx
+    transformMode: {
+      web: [/.[tj]sx$/]
+    }
+  }
 });
